b5d6086b1b31fcd842cbdd3b07d1149e389f21c1
emjwivell <emjwivell@gmail.com>
2016-02-08 17:02:38 -0500

    only three more sections to go

--- main.py
+++ main.py
@@ -38,3 +38,16 @@ def c_to_f(data):
     return [int((temp * 1.8) + 32) for temp in data]
 
 print(c_to_f(water_data_list))
+
+# dictionary date as key and wave height as value
+
+def d_and_wh(item):
+    day = [line.split(',')[-1] for line in item]
+    wave_height = [line.split(',')[1] for line in item]
+    n = 0
+    d_waveheight = {}
+    for _ in item:
+        d_waveheight[day[n]] = wave_height[n]
+        n += 1
+    return d_waveheight
+print(d_and_wh(contents))
